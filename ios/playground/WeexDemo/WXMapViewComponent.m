895166eb143eb55b99021723ba068aaa3990b1fe
guibao.gao <gaoguibao1921@gmail.com>
2016-12-22 16:05:39 +0800

    * [ios] update MAMapView alloc

--- ios/playground/WeexDemo/WXMapViewComponent.m
+++ ios/playground/WeexDemo/WXMapViewComponent.m
@@ -16,25 +16,13 @@
 
 @implementation WXMapViewComponent
 
-- (instancetype)initWithRef:(NSString *)ref type:(NSString *)type styles:(NSDictionary *)styles attributes:(NSDictionary *)attributes events:(NSArray *)events weexInstance:(WXSDKInstance *)weexInstance
-{
-    self = [super initWithRef:ref type:type styles:styles attributes:attributes events:events weexInstance:weexInstance];
-    if (self) {
-        
-        UIWindow *window = [UIApplication sharedApplication].keyWindow;
-        CGSize windowSize = window.rootViewController.view.frame.size;
-        self.mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, windowSize.width, windowSize.height)];
-        self.mapView.delegate = self;
-        
-    }
-    
-    return self;
-}
-
-- (UIView *)loadView {
+- (UIView *) loadView {
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    CGSize windowSize = window.rootViewController.view.frame.size;
+    self.mapView = [[MAMapView alloc] initWithFrame:CGRectMake(0, 0, windowSize.width, windowSize.height)];
+    self.mapView.delegate = self;
     
     return self.mapView;
 }
-
 
 @end
